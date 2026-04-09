410e081669506d3397ed76271ad6b84143b68d5e
Avilasha Goswami <avilashagoswami3@gmail.com>
2026-04-09 16:53:30 +0530

    Delete backend directory

--- backend/preprocess.py
+++
@@ -1,8 +0,0 @@
-def preprocess_image(image_path: str):
-    import cv2
-    from PIL import Image
-
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    return Image.fromarray(gray).convert("RGB")
